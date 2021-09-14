e717fa2b50613623c31f53d681931519497f0873
Максим Алексеев <maksimalekseev@Maksims-MacBook-Air.local>
2021-09-14 19:45:59 +0700

    Initial commit

---
+++ AvitoTestTask/Service/Networking/NetworkService.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkService.swift
+//  AvitoTestTask
+//
+//  Created by Максим Алексеев  on 14.09.2021.
+//
+
+import Foundation
+
+protocol NetworkService {
+    
+}
+
+class DefaultNetworkService: NetworkService {
+    // MARK:- Private 
+}
